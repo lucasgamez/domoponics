c43ca37bedcf593b78e0a02993dd20d2720fcc58
GAMEZ <lucasgamez@gmx.fr>
2023-03-15 15:30:41 -0400

    Update dht11_sensor.ino
    Final script for the esp module

--- scripts/dht11_sensor/dht11_sensor.ino
+++ scripts/dht11_sensor/dht11_sensor.ino
@@ -20,7 +20,7 @@ const String lux_sensor = "sensor_id=8";
 #define DHTPIN 12     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11 // DHT 11
 #define LED_PIN 14    // Digital pin connected to the communication led
-#define LUXPIN 16     // Analog led connected to the photoresistor
+#define LUXPIN A0     // Analog led connected to the photoresistor
 
 /******************************************
 * Wifi/http setup 
@@ -30,7 +30,7 @@ const String lux_sensor = "sensor_id=8";
 WiFiClient wifiClient;
 const char* ssid = "RollingPearl"; // Replace with your Wi-Fi network name
 const char* password = "Rolling1nTheWorld"; // Replace with your Wi-Fi network password
-const char* serverUrl = "http://192.0.0.112/sensors/"; // Replace with your Django server URL
+const char* serverUrl = "http://192.168.0.112:8000/sensors/add_sensor_data/"; // Replace with your Django server URL
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -74,7 +74,6 @@ void setup() {
 * 
 **/
 void loop() {
-  delay(2000); //Delay for sending data
 
   /******************/
   // Temperature and humidity
@@ -84,11 +83,6 @@ void loop() {
   dht.humidity().getEvent(&event);
   String humidity = "value=" + String(event.relative_humidity);
 
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" °C, Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
 
   //Get photo resistor value
   String photoResistorValue = "value=" + String(analogRead(LUXPIN));
@@ -96,17 +90,23 @@ void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     http.begin(wifiClient,serverUrl);
-    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
+    digitalWrite(LED_PIN, HIGH); 
+    delay(50);
+    digitalWrite(LED_PIN, LOW);
+    delay(50);
     int httpResponseCodeT = http.POST(humidity_sensor + "&" + humidity);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     int httpResponseCodeH = http.POST(temperature_sensor + "&" + temperature);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     int httpResponseCodeL = http.POST(lux_sensor + "&" + photoResistorValue);
 
     if (httpResponseCodeT > 0) {
       Serial.print("HTTP response code: ");
       Serial.println(httpResponseCodeT);
       digitalWrite(LED_PIN, HIGH); 
-      delay(100);
+      delay(50);
       digitalWrite(LED_PIN, LOW);
     } else {
       Serial.println("Error sending data to server.");
@@ -118,4 +118,6 @@ void loop() {
     Serial.println("WiFi connection lost.");
     digitalWrite(LED_PIN, HIGH); 
   }
+  
+  delay(1000*60); //Delay for sending data
 }
